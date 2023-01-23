0ee4a08b31eaef0edb3a737d0666b5ba6e45d6ad
bohjaangles <bohjaangles@gmail.com>
2023-01-23 16:08:41 -0600

    clean up the boilder plate that comes with CRA, add a header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../styles/header.scss'
+
+export default function Header() {
+  return (
+    <div className='header-wrapper'>
+      <div className="header-center-text">
+        <h3>Hello from Header!</h3>
+      </div>
+    </div>
+  )
+}
+
