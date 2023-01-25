de1a04313723afc02ca8afe0f18141c6260f9808
bohjaangles <bohjaangles@gmail.com>
2023-01-25 16:20:56 -0600

    add onclick function to hamburger menu icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,17 @@ import MenuIcon from '@mui/icons-material/Menu';
 import '../styles/header.scss'
 
 export default function Header() {
+  function hamburgler() {
+    alert('Would you like fries with that?')
+  }
+
   return (
     <div className='header-wrapper'>
       <div className="header-center-text">
         <h3>Hello from Header!</h3>
       </div>
       <div className="hamburger"
-        onClick={alert('do you want fries with that?')}
+        onClick={hamburgler}
       >
         <MenuIcon 
         />
