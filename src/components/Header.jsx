a78bb708961d0bfbda91af6d77c9d1d6fa949f5a
bohjaangles <bohjaangles@gmail.com>
2023-01-25 16:11:47 -0600

    add hamburger menu to top right of header, with an on hover animation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import MenuIcon from '@mui/icons-material/Menu';
 import '../styles/header.scss'
 
 export default function Header() {
@@ -6,6 +7,12 @@ export default function Header() {
     <div className='header-wrapper'>
       <div className="header-center-text">
         <h3>Hello from Header!</h3>
+      </div>
+      <div className="hamburger"
+        onClick={alert('do you want fries with that?')}
+      >
+        <MenuIcon 
+        />
       </div>
     </div>
   )
