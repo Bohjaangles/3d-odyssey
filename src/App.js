607c5f6938f78d10af99a15b973cb9f7db47e2ba
bohjaangles <bohjaangles@gmail.com>
2023-01-27 19:17:49 -0600

    change header from raw react to MUI

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import Header2 from './components/Header2';
 import CardList from './components/CardList';
 import './App.css';
 
 function App() {
   return (
     <div className="App" >
-      <Header/>
+      <Header2/>
       <CardList />
     </div>
   );
---
+++ src/components/Header2.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+
+export default function Header2() {
+  function hamburgler() {
+    alert('Would you like fries with that?')
+  }
+
+  return (
+    <Box sx={{ flexGrow: 1, boxShadow: 1 }}>
+      <AppBar position="static">
+        <Toolbar variant="dense">
+          <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+            <MenuIcon onClick={hamburgler}/>
+          </IconButton>
+          <Typography 
+            className='header-title'
+            variant="h6" 
+            color="inherit" 
+            component="div"
+            sx={{ ml: 87 }}
+          >
+            3D-Odyssey
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
